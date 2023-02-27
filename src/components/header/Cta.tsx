cb0a9d258cb45891179b54518f5b5f68f48d5167
toni <toni.salvado.rubio@gmail.com>
2023-02-27 18:38:27 +0100

    feat: dhange favicon and cv download link

--- src/components/header/Cta.tsx
+++ src/components/header/Cta.tsx
@@ -3,7 +3,11 @@ import React from "react";
 export function Cta() {
 	return (
 		<div className="header__cta">
-			<a href="../../assets/images/cv.pdf" download className="button">
+			<a
+				href="https://www.tonione.es/static/media/CV_Antonio_Salvadó.pdf"
+				download
+				className="button"
+			>
 				Descargar CV
 			</a>
 			<a href="#contact" className="button button--primary">
